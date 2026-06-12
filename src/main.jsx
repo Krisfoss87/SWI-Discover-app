fad09ffc99f02fd8941de43ac3c2b3dad3346feb
Krisfoss87 <Kristian.Brandt@swissinfo.ch>
2026-06-12 18:35:20 +0200

    Scaffold Vite project and move component to src/

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import DiscoverOddsChecker from "./DiscoverOddsChecker.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <DiscoverOddsChecker />
+  </React.StrictMode>
+);
